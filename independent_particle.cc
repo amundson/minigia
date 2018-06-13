156bad76c9fbf442b9386314430733d1eece00aa
James Amundson <amundson@fnal.gov>
2018-06-12 21:49:16 -0500

    working chef propagation

--- independent_particle.cc
+++ independent_particle.cc
@@ -197,6 +197,27 @@ propagate_omp_simd3(Bunch& bunch, libff_drift& thelibff_drift)
                          reference_momentum, m, reference_time);
     }
 }
+
+void
+propagate_omp_simd3_2(Bunch& bunch, libff_drift& thelibff_drift)
+{
+    const auto length = thelibff_drift.Length();
+    const auto reference_momentum =
+        bunch.get_reference_particle().get_momentum();
+    const auto m = bunch.get_mass();
+    const auto reference_time = thelibff_drift.getReferenceTime();
+
+    auto& particles(bunch.get_local_particles());
+    auto local_num = bunch.get_local_num();
+#pragma omp parallel for simd
+    for (Eigen::Index part = 0; part < local_num; ++part) {
+        libff_drift_unit(particles(part, Bunch::x), particles(part, Bunch::y),
+                         particles(part, Bunch::cdt),
+                         particles(part, Bunch::xp), particles(part, Bunch::yp),
+                         particles(part, Bunch::dpop), length,
+                         reference_momentum, m, reference_time);
+    }
+}
 #endif
 
 void
@@ -454,6 +475,9 @@ run()
               opt_timing, rank);
     run_check(&propagate_omp_simd3, "omp simd3", thelibff_drift, size, rank);
     do_timing(&propagate_omp_simd3, "omp simd3", bunch, thelibff_drift,
+              opt_timing, rank);
+    run_check(&propagate_omp_simd3_2, "omp simd3_2", thelibff_drift, size, rank);
+    do_timing(&propagate_omp_simd3_2, "omp simd3_2", bunch, thelibff_drift,
               opt_timing, rank);
 #endif
 }
