093c218687211f27485f0115693815be54a7259e
James Amundson <amundson@fnal.gov>
2018-07-27 13:33:08 -0500

    first pass of fft with original fftw-based scheme

--- fft-main.cc
+++ fft-main.cc
@@ -5,8 +5,9 @@
 #include "compare.h"
 #include "distributed_fft3d.h"
 
+template <typename T>
 void
-write_marray3dc(const char* filename, MArray3dc const& a)
+write_marray(const char* filename, T const& a)
 {
     std::ofstream file(filename);
     file << a.shape()[0] << "\n";
@@ -22,15 +23,16 @@ write_marray3dc(const char* filename, MArray3dc const& a)
     }
 }
 
-MArray3dc
-read_marray3dc(const char* filename)
+template <typename T>
+T
+read_marray(const char* filename)
 {
     std::ifstream file(filename);
     std::array<int, 3> cshape;
     file >> cshape[0];
     file >> cshape[1];
     file >> cshape[2];
-    MArray3dc a(boost::extents[cshape[0]][cshape[1]][cshape[2]]);
+    T a(boost::extents[cshape[0]][cshape[1]][cshape[2]]);
     for (unsigned long i = 0; i < a.shape()[0]; ++i) {
         for (unsigned long j = 0; j < a.shape()[1]; ++j) {
             for (unsigned long k = 0; k < a.shape()[2]; ++k) {
@@ -45,6 +47,7 @@ void
 run()
 {
     const std::vector<int> shape{ 32, 16, 8 };
+    //    const std::vector<int> shape{ 8, 4, 2 };
     Commxx_sptr commxx_sptr(new Commxx);
     Distributed_fft3d distributed_fft3d(shape, commxx_sptr, FFTW_ESTIMATE);
     auto lower = distributed_fft3d.get_lower();
@@ -60,8 +63,7 @@ run()
     for (int i = lower; i < upper; ++i) {
         for (int j = 0; j < shape[1]; ++j) {
             for (int k = 0; k < shape[2]; ++k) {
-                rarray[i][j][k] = 1.1 * k + 100 * j + 10000 * i;
-                orig[i][j][k] = 1.1 * k + 100 * j + 10000 * i;
+                orig[i][j][k] = rarray[i][j][k] = 1.1 * k + 100 * j + 10000 * i;
             }
         }
     }
@@ -73,9 +75,9 @@ run()
             .count();
     std::cout << "time = " << time << " s\n";
 
-    //    write_marray3dc("fft-carray.dat", carray);
-    auto check(read_marray3dc("fft-carray.dat"));
-    const double tolerance = 1.0e-15;
+    write_marray("fft-carray.dat", carray);
+    auto check(read_marray<MArray3dc>("fft-carray.dat"));
+    const double tolerance = 5.0e-11;
     std::cout << "check written: "
               << marray_check_equal(carray, check, tolerance) << std::endl;
     //    write_marray3dc("check.dat", check);
@@ -86,8 +88,29 @@ run()
         std::chrono::duration_cast<std::chrono::duration<double>>(end - start)
             .count();
     std::cout << "inverse time = " << time << " s\n";
+
+    double norm = shape[0] * shape[1] * shape[2];
+    for (int i = lower; i < upper; ++i) {
+        for (int j = 0; j < shape[1]; ++j) {
+            for (int k = 0; k < shape[2]; ++k) {
+                rarray[i][j][k] *= 1.0 / norm;
+            }
+        }
+    }
+
+    // zero out padded region
+    for (int i = lower; i < upper; ++i) {
+        for (int j = 0; j < rshape[1]; ++j) {
+            for (int k = shape[2]; k < rshape[2]; ++k) {
+                rarray[i][j][k] = 0.0;
+                orig[i][j][k] = 0.0;
+            }
+        }
+    }
+    //    write_marray("wtf-rarray.dat", rarray);
+    //    write_marray("wtf-orig.dat", orig);
     std::cout << "check roundtrip: "
-              << marray_check_equal(rarray, orig, tolerance);
+              << marray_check_equal(rarray, orig, tolerance) << std::endl;
 }
 
 int
