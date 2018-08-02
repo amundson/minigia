2345857f4f4ec6d874c189fec0be6b9605f26cbb
James Amundson <amundson@fnal.gov>
2018-08-02 11:32:40 -0500

    remove old commented-out code

--- fft-main.cc
+++ fft-main.cc
@@ -10,32 +10,6 @@
 #include "fftw++.h"
 #include "fmt/format.h"
 
-// unsigned int nx = 4, ny = 5, nz = 6;
-// unsigned int nzp = nz / 2 + 1;
-// size_t align = sizeof(Complex);
-
-// array3<Complex> F(nx, ny, nzp, align);
-// array3<double> f(nx, ny, nz, align); // For out-of-place transforms
-//// array3<double> f(nx,ny,2*nzp,(double *) F()); // For in-place transforms
-
-// rcfft3d Forward(nx, ny, nz, f, F);
-// crfft3d Backward(nx, ny, nz, F, f);
-
-// for (unsigned int i = 0; i < nx; i++)
-//    for (unsigned int j = 0; j < ny; j++)
-//        for (unsigned int k = 0; k < nz; k++)
-//            f(i, j, k) = i + j + k;
-
-// cout << "\ninput:\n" << f;
-
-// Forward.fft(f, F);
-
-// cout << "\noutput:\n" << F;
-
-// Backward.fftNormalized(F, f);
-
-// cout << "\nback to input:\n" << f;
-
 typedef std::array<int, 3> Shape_t;
 
 template <typename T>
